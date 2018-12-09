099d4bb5bb6220f613d701ff836022c7dab87a57
jmzobitz <zobitz@augsburg.edu>
2018-12-08 21:28:11 -0600

    updated single_triangle

--- R/single_triangle.R
+++ R/single_triangle.R
@@ -2,11 +2,12 @@
 #'
 #' \code{single_triangle} Computes the degree days from temperature data, using interpolation from 
 #'
-#' @param min_t Minimum daily temperature
-#' @param max_t Maximum daily temperature
+#' @param min_temp Minimum daily temperature
+#' @param max_tempemp Maximum daily temperature
 #' @param t_L Lower bound on the temperature
 #' @param t_U Upper bound on the temperature
 #' @seealso For the equations to compute the degree day: \url{http://ipm.ucanr.edu/WEATHER/ddst_tbl.html}
+#' @source 
 #' @return Degree day from the list of numbers.
 #' @examples
 #'
@@ -14,18 +15,18 @@
 
 #' @export
 
-single_triangle <-function(min_t,max_t,t_L,t_U) {
-  if (min_t!=max_t) {
-    if(t_L < min_t & t_U < max_t) {  # Intercepted by the upper threshold.
-      degree_day <- 6*(max_t+min_t-2*t_L)/12 -(6*(max_t-t_U)^2/(max_t-min_t))/12
-    } else if( min_t <= t_L & t_U < max_t) {  # Intercepted by both thresholds.
-      degree_day = (6*(max_t-t_L)^2/(max_t-min_t) - 6*(max_t-t_U)^2/(max_t-min_t) )/12
-    } else if(t_L < min_t & max_t <= t_U ) {  # Entirely between both thresholds.
-      degree_day <- 6*(max_t + min_t -2*t_L)/12
-    } else if(min_t <= t_L & max_t <= t_U) { #Intercepted by the lower threshold
-      degree_day <- 6*(max_t- t_L)^2/(max_t-min_t)/12
-    } else if(t_U < min_t) {degree_day <- t_U-t_L} #Completely above both thresholds
-    else if(max_t < t_L) {degree_day <- 0} #Completely below both thresholds
+single_triangle <-function(min_temp,max_temp,t_L,t_U) {
+  if (min_temp!=max_temp) {
+    if(t_L < min_temp & t_U < max_temp) {  # Intercepted by the upper threshold.
+      degree_day <- 6*(max_temp+min_temp-2*t_L)/12 -(6*(max_temp-t_U)^2/(max_temp-min_temp))/12
+    } else if( min_temp <= t_L & t_U < max_temp) {  # Intercepted by both thresholds.
+      degree_day = (6*(max_temp-t_L)^2/(max_temp-min_temp) - 6*(max_temp-t_U)^2/(max_temp-min_temp) )/12
+    } else if(t_L < min_temp & max_temp <= t_U ) {  # Entirely between both thresholds.
+      degree_day <- 6*(max_temp + min_temp -2*t_L)/12
+    } else if(min_temp <= t_L & max_temp <= t_U) { #Intercepted by the lower threshold
+      degree_day <- 6*(max_temp- t_L)^2/(max_temp-min_temp)/12
+    } else if(t_U < min_temp) {degree_day <- t_U-t_L} #Completely above both thresholds
+    else if(max_temp < t_L) {degree_day <- 0} #Completely below both thresholds
     
   } else {degree_day <- 0}
   
